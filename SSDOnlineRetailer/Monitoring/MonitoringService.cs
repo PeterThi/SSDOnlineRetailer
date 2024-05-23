5afb1582589cd0208bb79a104231d9409e8c192d
PeterThi <pete9817@gmail.com>
2024-05-23 15:18:57 +0200

    Init with monitor

---
+++ SSDOnlineRetailer/Monitoring/MonitoringService.cs
@@ -0,0 +1,34 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+using Serilog.Core;
+using System.Diagnostics;
+using System.Reflection;
+namespace Monitoring
+   
+{
+    public static  class MonitoringService
+    {
+
+        public static ILogger Log => Serilog.Log.Logger;
+
+        static MonitoringService()
+        {
+
+            //Serilog stuff
+            Serilog.Log.Logger = new LoggerConfiguration()
+
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://seq")
+                .CreateLogger();
+
+            Log.Debug("Started Logger in MonitorService");
+            //Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine($"Serilog: {msg}"));
+        }
+        
+
+
+    }
+}
